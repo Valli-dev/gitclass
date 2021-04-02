1bcf7f3f0e63f78399dcf1b4fe61d0f308d3f369
Valli-dev <vallimayilannamalai@gmail.com>
2021-04-02 13:18:06 -0700

    updated from github desktop
    This is with added console table

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("this is a java script file");
 console.table({'name': 'Valli'});
 console.log("I cloned js file and now adding a console at locally");
+console.log(" this is from githubDesktop");
